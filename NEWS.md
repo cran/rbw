1edd73bd2f76810c6fc454d078c6dd43eab5e51d
Xiang Zhou <xiang_zhou@fas.harvard.edu>
2022-02-27 22:10:02 +0000

    version 0.3.1

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,10 @@
+# rbw 0.3.1
+
+* renamed the rbwATE() function to rbwPoint()
+* added more argument checks
+* added automated unit tests
+* added CITATION file
+
 # rbw 0.3.0
 
 * added the rbwATE() function for estimating the average treatment effect (ATE)
--- R/rbwPoint.R
+++ R/rbwPoint.R
@@ -1,67 +1,82 @@
-#' Residual Balancing Weights for Estimating the Average Treatment Effect (ATE)
+#' Residual Balancing Weights for Estimating the Average Treatment Effect (ATE) in a Point Treatment Setting
 #'
-#' \code{rbwATE} is a function that produces residual balancing weights for
-#' estimating the average treatment effect (ATE). The weights can be used to fit marginal
-#' structural models for the effect of the treatment on the outcome.
+#' \code{rbwPoint} is a function that produces residual balancing weights in a point treatment setting. It takes
+#' a set of baseline confounders and computes the residuals for each confounder by centering it around
+#' its sample mean. The weights can be used to fit marginal structural models to estimate the average treatment
+#' effect (ATE).
 #'
-#' @param treatment A symbol or character string for the treatment variable.
-#' @param baseline_x An expression for a set of baseline confounders stored in \code{data}.
+#' @param treatment A symbol or character string for the treatment variable in \code{data}.
+#' @param baseline_x An expression for a set of baseline confounders stored in \code{data} or a character
+#'  vector of the names of these variables.
 #' @inheritParams rbwMed
 #'
 #' @return A list containing the results.
 #'  \item{weights}{A vector of residual balancing weights.}
 #'  \item{constraints}{A matrix of (linearly independent) residual balancing constraints}
-#'  \item{eb_out}{Results from calling \code{\link{eb2}} function}
+#'  \item{eb_out}{Results from calling the \code{\link{eb2}} function}
 #'  \item{call}{The matched call.}
 #' @export
 #'
 #' @examples
 #' # residual balancing weights
-#' rbwATE_fit <- rbwATE(treat, baseline_x = c(log_TotalPop, PercentOver65, log_Inc,
+#' rbwPoint_fit <- rbwPoint(treat, baseline_x = c(log_TotalPop, PercentOver65, log_Inc,
 #'   PercentHispanic, PercentBlack, density,
 #'   per_collegegrads, CanCommute), data = advertisement)
 #'
 #' # attach residual balancing weights to data
-#' advertisement$rbw_ate <- rbwATE_fit$weights
+#' advertisement$rbw_point <- rbwPoint_fit$weights
 #'
 #' # fit marginal structural model
 #' if(require(survey)){
-#'   rbw_design <- svydesign(ids = ~ 1, weights = ~ rbw_ate, data = advertisement)
+#'   rbw_design <- svydesign(ids = ~ 1, weights = ~ rbw_point, data = advertisement)
 #'   # the outcome model includes the treatment, the square of the treatment,
 #'   # and state-level fixed effects (Fong, Hazlett, and Imai 2018)
-#'   msm_rbwATE <- svyglm(Cont ~ treat + I(treat^2) + factor(StFIPS), design = rbw_design)
-#'   summary(msm_rbwATE)
+#'   msm_rbwPoint <- svyglm(Cont ~ treat + I(treat^2) + factor(StFIPS), design = rbw_design)
+#'   summary(msm_rbwPoint)
 #' }
 #'
-rbwATE <- function(treatment, data, baseline_x, base_weights,
-                   max_iter = 200, print_level = 1, tol = 1e-6) {
+rbwPoint <- function(treatment, data, baseline_x, base_weights,
+                   max_iter = 200, tol = 1e-4, print_level = 1) {
 
   # match call
   cl <- match.call()
 
-  # check treatment
+  # check and quote treatment
   if(missing(treatment)) stop("'treatment' must be provided.")
+  if(!(typeof(enexpr(treatment)) %in% c("symbol", "character"))){
+    stop("'treatment' must be a symbol or character string")
+  }
   treatment <- ensym(treatment)
 
   # check data
   if(missing(data)) stop("'data' must be provided.")
-  if(!is.data.frame(data)) stop("'data' must be a data frame.")
+  if(!is.data.frame(data) || nrow(data) < 2) stop("'data' must be a data frame with at least 2 rows.")
   n <- nrow(data)
+
+  # check if treatment is in data
+  treatment_name <- as_string(treatment)
+  if(!(treatment_name %in% names(data))) stop(paste0(treatment_name, " is not in 'data'"))
 
   # check base weights
   if(missing(base_weights)){
     bweights <- rep(1, n)
   } else{
     bweights <- eval_tidy(enquo(base_weights), data)
-    if(length(bweights) != n) stop("'base_weights' must have the same length as 'data'.")
+    if(length(bweights) != n || !is.double(bweights)){
+      stop("'base_weights' must be numeric and have the same length as 'data'.")
+    }
   }
 
   # construct xmodels for baseline confounders
   if(!missing(baseline_x)) {
+    baseline_x <- enquo(baseline_x)
     nl <- as.list(seq_along(data))
     names(nl) <- names(data)
-    vars <- eval_tidy(enexpr(baseline_x), nl)
-    xnames <- names(data)[vars]
+    vars <- eval_tidy(baseline_x, nl)
+    if(!(is.character(vars) || is.integer(vars))){
+      stop("'baseline_x' should either be a character vector or an expression of baseline confounders in 'data'")
+    }
+    xnames <- if(is.character(vars)) vars else names(data)[vars]
     xform <- paste("~", paste(xnames, collapse = "+"))
     x <- model.matrix(eval_tidy(parse_expr(xform)), data)[, -1, drop = FALSE]
     xmodels <- apply(x, 2, function(y) lm(y ~ 1, weights = bweights))
